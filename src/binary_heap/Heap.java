b8a8e3097d732d841cb226b566e94022538ab4c4
gonzalo <gibarsin@itba.edu.ar>
2015-10-04 20:35:02 -0300

    added heap interface

---
+++ src/binary_heap/Heap.java
@@ -0,0 +1,13 @@
+package binary_heap;
+
+/**
+ * Created by gonzalo on 10/4/15.
+ * Public interface for heap
+ */
+public interface Heap {
+    void insert(int n);
+
+    int delete();
+
+    void print();
+}
